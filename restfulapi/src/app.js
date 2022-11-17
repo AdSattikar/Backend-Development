9be3ea08bb31b441265aba889df8eaf0b7d659e4
Adnan Sattikar <adnan.sattikar2001@gmail.com>
2022-11-17 22:46:30 +0530

    connected db, created schema & model

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -1,11 +1,15 @@
 const express = require('express')
+require("./db/conn")
+
+const Student = require("./models/students");
 const app = express();
 const port = process.env.PORT || 3000
 
-app.get("/",(req,res)=>{
-    res.send("Hello")
-})
+// app.get("/",(req,res)=>{
+//     res.send("Hello")
+// })
 
+//create new students
 app.post("/students",(req,res)=>{
     res.send("Hello")
 })
