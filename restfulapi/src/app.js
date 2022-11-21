175e951f3b3b277f05e1462e4715bd19cd01940d
Adnan Sattikar <adnan.sattikar2001@gmail.com>
2022-11-21 16:33:29 +0530

    API CRUD Operations

--- restfulapi/src/app.js
+++ restfulapi/src/app.js
@@ -7,9 +7,6 @@ const port = process.env.PORT || 3000
 
 app.use(express.json())
 
-// app.get("/",(req,res)=>{
-//     res.send("Hello")
-// })
 
 //create new students
 // app.post("/students", (req, res) => {
@@ -35,5 +32,56 @@ app.post("/students", async (req, res) => {
     }
 })
 
+app.get("/students", async(req,res)=>{
+  try{
+   const studentsData = await Student.find()
+   res.send(studentsData)
+  }catch(e){
+    res.send(e)
+  }
+})
 
+// get individual student 
+app.get("/students/:_id", async(req,res)=>{
+    try {
+        const _id = req.params._id;
+        const indiStudent = await Student.findById({_id})
+        console.log(indiStudent)
+
+        if(!indiStudent){
+            return res.status(404).send();
+        }else{
+            res.send(indiStudent)
+        }
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+//update student by id
+app.patch("/students/:id", async(req,res)=>{
+    try {
+        const id = req.params.id;
+        const updateStudent = await Student.findByIdAndUpdate(id,req.body,{new:true});
+        console.log(updateStudent)
+        res.send(updateStudent)
+        
+    } catch (e) {
+        res.status(400).send(e)
+    }
+})
+
+// delete student by id
+
+app.delete("/students/:id", async(req,res) => {
+    try{
+        const deleteStudent = await Student.findByIdAndDelete(req.params.id);
+        if(!req.params.id){
+            return res.status(400).send();
+        }
+        res.send("deleting student"+deleteStudent)
+    }catch(e){
+        res.status(500).send(e)
+    }
+})
 app.listen(port, () => console.log(`Server running at port ${port}`));
